5de91b49dbe6bc4d8858608055b64e12ba7eb1db
Jason Cartwright <jason.cartwright44@gmail.com>
2019-02-09 12:33:26 -0800

    api changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,10 +36,10 @@ if (search !== '') {
 
 
 
-// $.ajax({
-//     url: queryURL,
-//     method: 'GET'
-// }).then(function (response) {
+$.ajax({
+    url: queryURL,
+    method: 'GET'
+}).then(function (response) {
 
 
     console.log(queryURL);
@@ -54,21 +54,21 @@ if (search !== '') {
 // Eventbrite API. The first ajax gets the basic info, while the seond one gets the location info. For 'VenueID' the number has to be changed  to whichever item you're looking at on the page.
 
 
-// var city = 'Sacramento';
+var city = '';
+var name = 'Sactown Union Brewery';
+var name2 = name.replace(/ /g,"+");
 
-
-
+if (name !== '') {
 var tokenEB = 'LTCWX6ZN5R4U7VWXPNCG';
-var queryURLEB = 'https://www.eventbriteapi.com/v3/events/search/?token=' + tokenEB + '&location.address=' + city ;
+var queryURLEB = 'https://www.eventbriteapi.com/v3/events/search/?token=' + tokenEB + '&q=' + name2  + '&location.address=' + city;
 
 
-// $.ajax({
-//     url: queryURLEB,
-//     method: 'GET'
-// }).then(function (response) {
-
-//     console.log(response);
-
-
+$.ajax({
+    url: queryURLEB,
+    method: 'GET'
+}).then(function (response) {
+    console.log(response.events);
 })
+}
+
 
