c81e23a0818a29113675f8df23961a195e8f44c5
pcruz0807 <codecruz86@gmail.com>
2019-02-09 12:50:12 -0800

    firebase info

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,14 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB95HHvt9Y67FcllrvjcNvMjYd8bGJK518",
+    authDomain: "fir-cruz.firebaseapp.com",
+    databaseURL: "https://fir-cruz.firebaseio.com",
+    projectId: "fir-cruz",
+    storageBucket: "fir-cruz.appspot.com",
+    messagingSenderId: "192419918937"
+  };
+  firebase.initializeApp(config);
+
+  
+
+  
